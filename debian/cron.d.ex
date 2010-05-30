5657445dd7f878cc408770ba2c0357d3d488f7b7
Javier Candeira <javier@candeira.com>
2010-05-30 23:07:07 +1000

    removed /debian from master

--- debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the mirrorstage package
-#
-0 4	* * *	root	[ -x /usr/bin/mirrorstage_maintenance ] && /usr/bin/mirrorstage_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(mirrorstage):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="mirrorstage" command="/usr/bin/mirrorstage"
